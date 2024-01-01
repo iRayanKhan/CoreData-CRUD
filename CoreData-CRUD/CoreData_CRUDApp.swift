8f9d7d0ee1db677247626af826879a8f823e1e2a
Rayan Khan <iRayanKhan@icloud.com>
2024-01-01 04:12:43 -0600

    Initial Commit

---
+++ CoreData-CRUD/CoreData_CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_CRUDApp.swift
+//  CoreData-CRUD
+//
+//  Created by Rayan Khan on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
